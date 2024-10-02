4299118346473e6ebdd6c19e4d2aa4ecb51c31a1
sonuvairagi <sonuv@fss.co.in>
2024-10-02 12:15:11 +0530

    commiting video_streaming_app project structure

---
+++ VideoStreaming/src/test/java/com/VideoStreaming/VideoStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VideoStreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
